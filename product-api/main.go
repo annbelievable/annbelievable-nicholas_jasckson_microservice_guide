f527a14dd86c208276845c8f8915bf301adfa1b0
kokann <kokann_92@hotmail.com>
2022-02-07 22:59:35 +0800

    Fix typo and add some comment

--- product-api/main.go
+++ product-api/main.go
@@ -60,12 +60,12 @@ func main() {
 
 	// create a new server
 	s := http.Server{
-		Addr:         *bindAddress,
-		Handler:      ch(sm),
-		ErrorLog:     l,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  120 * time.Second,
+		Addr:         *bindAddress,      // configure the bind address
+		Handler:      ch(sm),            // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
 	}
 
 	// start the server
@@ -88,7 +88,7 @@ func main() {
 	sig := <-c
 	log.Println("Got signal:", sig)
 
-	// gracefully shutdown the server, waiting max 30 seconds for current opertations to complete
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
 	s.Shutdown(ctx)
 }
